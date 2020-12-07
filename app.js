5d848da86b3b119e581679e6d9af515571df486e
etcetc1820 <etcetc1820@gmail.com>
2020-12-07 17:01:57 +0200

    init

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+const mongooseUrl = process.env.MONGO_DB_URL;
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS, PATCH"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  next();
+});
+
+mongoose
+  .connect(mongooseUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port);
+    console.log("server is running");
+  })
+  .catch((err) => console.log(err));
